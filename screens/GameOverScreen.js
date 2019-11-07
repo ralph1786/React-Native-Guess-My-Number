53b4147de11eba17498b375bd2302d56dec406ca
ralph1786 <ralph1786@gmail.com>
2019-11-07 13:16:13 -0500

    added image, number of rounds

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import CustomButton from "../components/CustomButton";
 
 const GameOverScreen = ({ numberRounds, restartGameHandler }) => {
   return (
     <View style={styles.screen}>
-      <Text>Game Over!</Text>
-      <Text>Number of Rounds: {numberRounds}</Text>
-      <Button title="NEW GAME" onPress={restartGameHandler} />
+      {/* <Text style={}>Game Over!</Text> */}
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/game-over.png")}
+          resizeMode="cover"
+        />
+      </View>
+      <Text style={styles.textContainer}>
+        Number of Rounds: <Text style={styles.highlight}>{numberRounds}</Text>
+      </Text>
+      <CustomButton onPress={restartGameHandler}>NEW GAME</CustomButton>
     </View>
   );
 };
@@ -16,6 +26,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    marginVertical: 30
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  },
+  highlight: {
+    color: "red",
+    fontWeight: "600"
+  },
+  textContainer: {
+    textAlign: "center",
+    fontSize: 22,
+    marginVertical: 15
   }
 });
 
